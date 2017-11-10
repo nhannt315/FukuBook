ab525dcd0eb13c7806d42d7fe621ade20a623388
yungwiche <le.minhhoang@outlook.com>
2017-11-10 20:51:59 +0700

    Fix post prototype: - Post controller route

--- server/modules/api/posts/postController.js
+++ server/modules/api/posts/postController.js
@@ -5,23 +5,24 @@ const config = require('../../../config.json');
 const postModel = require('./postModel.js');
 
 Router.get('/:category/:page', (req, res) => {
-  postModel.getPostsFromDBWithCategory(req.params.category, req.params.page, (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
-});
-
-Router.get('/all/:page', (req, res) => {
-  postModel.getAllPostsFromDB(req.params.page, (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(result);
-    }
-  });
+  if (req.params.category == "all"){
+    postModel.getAllPostsFromDB(req.params.page, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  }
+  else{
+    postModel.getPostsFromDBWithCategory(req.params.category, req.params.page, (err, result) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send(result);
+      }
+    });
+  }
 });
 
 module.exports = Router;
